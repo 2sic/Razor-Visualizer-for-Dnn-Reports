40b6ee31193dfc0fd78da67d2db2cb6c687b0f15
iJugleboy <daniel.mettler@2sic.com>
2015-03-24 08:01:00 +0100

    Added base VB Solution

---
+++ Razor/Settings.ascx.vb
@@ -0,0 +1,42 @@
+﻿
+Imports DotNetNuke.Modules.Reports.Extensions
+Imports System.Collections.Generic
+Imports DotNetNuke.Entities.Modules
+
+Namespace DotNetNuke.Modules.Reports.Visualizers.Razor
+    Partial Class Settings
+        Inherits ReportsSettingsBase
+
+        Private RazorSettings As RazorHost.Settings
+
+        Protected Sub Page_Init (ByVal sender As Object, ByVal e As EventArgs) Handles Me.Init
+            LoadRazorSettingsControl()
+            EnsureEditScriptControlIsRegistered()
+        End Sub
+
+        Public Overrides Sub LoadSettings (ByVal Settings As Dictionary(Of String, String))
+            RazorSettings.LoadSettings()
+        End Sub
+
+        Public Overrides Sub SaveSettings (ByVal Settings As Dictionary(Of String, String))
+            RazorSettings.UpdateSettings()
+        End Sub
+
+        Private Sub EnsureEditScriptControlIsRegistered()
+            Dim moduleDefId As Integer = ParentModule.ModuleConfiguration.ModuleDefID
+            If ModuleControlController.GetModuleControlByControlKey("EditRazorScript", moduleDefId) Is Nothing Then
+                Dim m As ModuleControlInfo
+                m = New ModuleControlInfo With {.ControlKey = "EditRazorScript", .ControlSrc = "DesktopModules/RazorModules/RazorHost/EditScript.ascx", .ControlTitle = "Edit Script", _
+                    .ControlType = SecurityAccessLevel.Host, .ModuleDefID = moduleDefId}
+                ModuleControlController.UpdateModuleControl(m)
+            End If
+        End Sub
+
+        Private Sub LoadRazorSettingsControl()
+            RazorSettings = CType (LoadControl ("~/DesktopModules/RazorModules/RazorHost/Settings.ascx"), RazorHost.Settings)
+            RazorSettings.ModuleConfiguration = ParentModule.ModuleConfiguration
+            RazorSettings.LocalResourceFile = Me.LocalResourceFile
+            Controls.Add (RazorSettings)
+        End Sub
+    End Class
+End Namespace
---
+++ Razor/Visualizer.ascx
@@ -0,0 +1,1 @@
+﻿<%@ Control Language="VB" AutoEventWireup="false" CodeFile="Visualizer.ascx.vb" Inherits="DotNetNuke.Modules.Reports.Visualizers.Razor.Visualizer" %>                
